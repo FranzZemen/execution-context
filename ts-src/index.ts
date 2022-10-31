d8e327fa8d8c3ec4d5edc732005cc3aed25b8d50
Franz Zemen <franzzemen@gmail.com>
2022-10-30 21:15:42 -0400

    schema Execution created

--- ts-src/index.ts
+++ ts-src/index.ts
@@ -8,7 +8,7 @@ export class ExecutionContextDefaults {
   static Authorization = 'None';
   static LocalContext = 'None';
 
-  static Execution() {
+  static Execution() : Execution {
     return {
       thread: `${ExecutionContextDefaults.Thread} ${uuidv4()}`,
       requestId: `${ExecutionContextDefaults.RequestId} ${uuidv4()}`,
@@ -24,51 +24,52 @@ export class ExecutionContextDefaults {
   }
 }
 
+export interface Execution {
+  thread?: string; // The thread of execution, which can span across processes (if supplied in headers).
+  requestId?: string; // The current process request id, as formed from  the aws request id or some other source
+  authorization?: string; // Optional authorization in Bearer Token format Bearer [jwt]
+  localContext?: string; // Optional local context - user beware of tracking this!
+}
+
 
 export interface ExecutionContext {
   // We nest in its own field for downward compatibility
-  execution?: {
-    thread?: string; // The thread of execution, which can span across processes (if supplied in headers).
-    requestId?: string; // The current process request id, as formed from  the aws request id or some other source
-    authorization?: string; // Optional authorization in Bearer Token format Bearer [jwt]
-    localContext?: string; // Optional local context - user beware of tracking this!
-  };
+  execution?: Execution;
   validated?: boolean;
 }
 
-export const executionContextSchema: ValidationSchema = {
-  execution: {
-    type: 'object',
+export const executionSchema: ValidationSchema = {
+  thread: {
+    type: 'string',
     optional: true,
-    default: ExecutionContextDefaults.Execution(),
-    props: {
-      thread: {
-        type: 'string',
-        optional: true,
-        default: ExecutionContextDefaults.Thread
-      },
-      requestId: {
-        type: 'string',
-        optional: true,
-        default: ExecutionContextDefaults.RequestId
-      },
-      authorization: {
-        type: 'string',
-        optional: true,
-        default: ExecutionContextDefaults.Authorization
-      },
-      localContext: {
-        type: 'string',
-        optional: true,
-        default: ExecutionContextDefaults.LocalContext
-      },
-      checked: {
-        type: 'boolean',
-        optional: true,
-        default: true // If checked/defaulted
-      }
-    }
+    default: ExecutionContextDefaults.Thread
   },
+  requestId: {
+    type: 'string',
+    optional: true,
+    default: ExecutionContextDefaults.RequestId
+  },
+  authorization: {
+    type: 'string',
+    optional: true,
+    default: ExecutionContextDefaults.Authorization
+  },
+  localContext: {
+    type: 'string',
+    optional: true,
+    default: ExecutionContextDefaults.LocalContext
+  }
+}
+
+export const executionSchemaWrapper: ValidationSchema = {
+  type: 'object',
+  optional: true,
+  default: ExecutionContextDefaults.Execution(),
+  props: executionSchema
+}
+
+export const executionContextSchema: ValidationSchema = {
+  execution: executionSchemaWrapper,
   validated: {
     type: 'boolean',
     optional: true,
